0dee6eb9a91f12f787c466d64bd8af6ef503042f
David <david.llobet6@gmail.com>
2021-11-10 18:21:28 +0100

    Create index.js in server folder

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const cors = require("cors");
+const debug = require("debug")("robots:server");
+
+const express = require("express");
+const morgan = require("morgan");
+
+
+const app = express();
+
+const initializeServer = (port) => 
+    new Promise((resolve, reject) => {
+        const server = app.listen(port, () => {
+            debug(`Escuchando en el puerto ${port}`);
+            resolve(server);
+        });
+        server.on("error", (error) => {
+            debug("Ha habido un error al iniciar el servidor");
+            if (error.code === "EADDRINUSE") {
+                debug(`El puerto ${port} está en uso.`);
+            }
+            reject();
+        });
+        server.on("close", () => {
+            debug("Servidor express desconectado");
+        });
+    });
+
+    app.use(morgan("dev"));
+    app.use(cors());
+    app.use(express.json());
+
+    module.exports = { app, initializeServer };

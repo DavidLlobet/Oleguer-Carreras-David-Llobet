9b1115e6263cf253d3a72d8a241290b6849f5502
David <david.llobet6@gmail.com>
2021-11-10 20:40:23 +0100

    Create basic structure for series controllers

---
+++ server/controllers/seriesController.js
@@ -0,0 +1,19 @@
+const Serie = require("../../database/models/serie");
+
+const getAllSeries = async (req, res, next) => {};
+
+const getViewedSeries = async (req, res, next) => {};
+
+const getPendingSeries = async (req, res, next) => {};
+
+const createSerie = async (req, res, next) => {};
+
+const modifySerie = async (req, res, next) => {};
+
+const deleteSerie = async (req, res, next) => {};
+
+const markViewedSerie = async (req, res, next) => {};
+
+module.exports = {
+    getAllSeries, getViewedSeries, getPendingSeries, createSerie, modifySerie, deleteSerie, markViewedSerie
+}
